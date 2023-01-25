86e5e650e146806387badf29a543a5041c387a9f
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-01-25 18:05:12 +0530

    replaced separate function with arrow function

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-function changer() {
-    const str = "0123456789ABCDEF";
-    const rand = [];
-    rand[0] = "#";
-    for (let i = 1; i <= 6; i++) {
-        rand[i] = str[Math.floor(Math.random() * 16)];
-    }
-    const newColor = rand.join("");
-    document.getElementById("canvas").style.backgroundColor = newColor;
-}
-
-document.getElementById("button").addEventListener("click", changer);
+document.getElementById("button").addEventListener("click", () => {
+  const str = "0123456789ABCDEF";
+  const rand = [];
+  rand[0] = "#";
+  for (let i = 1; i <= 6; i++) {
+    rand[i] = str[Math.floor(Math.random() * 16)];
+  }
+  const newColor = rand.join("");
+  document.getElementById("canvas").style.backgroundColor = newColor;
+});
